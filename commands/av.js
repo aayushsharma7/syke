1c28fc09d5f5334580b02da19c1b109291c5c71a
Hedrie7 <65645066+Hedrie7@users.noreply.github.com>
2020-08-22 16:42:32 +0530

    gchtfgjhmbn

--- commands/av.js
+++ commands/av.js
@@ -4,11 +4,13 @@ module.exports = {
     name: 'av',
     description: "says av!",
     execute(msg, args){
+        const member = msg.guild.member(user);
         const embed = new Discord.MessageEmbed()
+        
         .setTitle('Avatar')
         .setAuthor(msg.author.username)
         .setColor('RANDOM')
-        .setImage(msg.user.displayAvatarURL())
+        .setImage(msg.member.displayAvatarURL())
 
     msg.channel.send(embed);
     }
