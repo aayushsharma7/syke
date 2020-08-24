8478be00170ba9836cc59e7ac334d94c4ca8f44f
Hedrie7 <65645066+Hedrie7@users.noreply.github.com>
2020-08-24 11:03:44 +0530

    hello

--- commands/av.js
+++ commands/av.js
@@ -1,16 +1,27 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const bot = new Discord.Client()
+=;
 module.exports = {
     name: 'av',
     description: "says av!",
-    execute(msg, args){
-    
-        const embed = new Discord.MessageEmbed()
+    execute: async(msg, args)=> {
+        let user;
+  
+  if (msg.mentions.users.first()) {
+    user = msg.mentions.users.first();
+  } else if (args[0]) {
+    user = msg.guild.members.cache.get(args[0]).user;
+  } else {
+    user = msg.author;
+  }
+  let avatar = user.displayAvatarURL({size: 4096, dynamic: true});
+  const embed = new Discord.MessageEmbed()
         
-        .setTitle('Avatar')
-        .setAuthor(msg.author.username)
-        .setColor('RANDOM')
-        .setImage(msg.author.displayAvatarURL())
+  .setTitle(`${user.tag} avatar`)
+  .setDescription(`[Avatar URL of **${user.tag}**](${avatar})`)
+  .setColor('RANDOM')
+  .setImage(avatar)
+  
 
     msg.channel.send(embed);
     }
