5d6b07a26216d89776e2d5dde73e17e7e09b480e
hedrie7 <65645066+Hedrie7@users.noreply.github.com>
2020-08-27 10:46:03 +0530

    ah

--- commands/dc.js
+++ commands/dc.js
@@ -13,7 +13,7 @@ module.exports = {
         const voiceChannel = msg.member.voice.channel
         if(!msg.member.voice.channel) return msg.channel.send("You need to be in a voice channel to disconnect ")
         msg.member.voice.channel.leave()
-        return undefined
+        return msg.channel.send("Disconnected from the voice channel")
         
 
         
